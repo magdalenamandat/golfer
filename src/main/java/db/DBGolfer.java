ea38a341a9c3bdbe906fc84fa32ba1266d43bd14
Magdalena <m.mandat@gmail.com>
2018-07-09 21:48:34 +0100

    method save added

--- src/main/java/db/DBGolfer.java
+++ src/main/java/db/DBGolfer.java
@@ -1,5 +1,7 @@
 package db;
 
+import models.Golfer;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -7,4 +9,18 @@ public class DBGolfer {
 
     private static Session session;
     private static Transaction transaction;
+
+    public static void save(Golfer golfer) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.save(golfer);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 }

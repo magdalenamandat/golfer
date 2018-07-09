d637a924039c2f600a7bc0bc9b1dfe938b799c54
Magdalena <m.mandat@gmail.com>
2018-07-09 21:46:25 +0100

    session transaction added

--- src/main/java/db/DBGolfer.java
+++ src/main/java/db/DBGolfer.java
@@ -1,4 +1,10 @@
 package db;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class DBGolfer {
+
+    private static Session session;
+    private static Transaction transaction;
 }
